a821c2b2b719e570ef634c0f3d806e1fa8002388
Xirdim <xiralist@gmail.com>
2021-08-02 23:47:35 +0900

    removed retweets from tweet list.

--- scripts/extract.py
+++ scripts/extract.py
@@ -1,6 +1,10 @@
 import re
 
 def extract_tweets(src_path, encoding='UTF-8'):
+  '''
+  一部の記号等に対応していない可能性があります。
+  なるべく既に作ってあるテキストファイルを使用し、この関数を積極的には使用しないでください。
+  '''
   res = []
   with open(src_path, encoding=encoding) as src:
     for line in src:
